3c927d710373f961c02eeb1495f5cba6c2118da2
Raon98 <cheol1998@kakao.com>
2022-10-20 09:36:21 +0900

    13 shouldComponentUpdate 작성

--- src/component/phone/PhoneInfoList.js
+++ src/component/phone/PhoneInfoList.js
@@ -7,6 +7,11 @@ class PhoneInfoList extends Component{
         onRemove: () => console.warn('onRemove not defined'),
         onUpdate: () => console.warn('onUpdate not defined')
     }
+
+    shouldComponentUpdate (nextProps,nextState){
+    //다음 받아올 data 가 현재 data 랑 다른 배열일 때 true 로 설정
+        return nextProps.data !== this.props.data
+    }
     render() {
         /**
          * 배열을 랜더링할시에는 꼭 KEY값을 사용해야함
