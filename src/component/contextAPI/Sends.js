093f4fac4288134c0e675d1f33b2eb490917881d
Raon98 <cheol1998@kakao.com>
2022-11-01 14:55:11 +0900

    CONTEXT API HoC 사용

--- src/component/contextAPI/Sends.js
+++ src/component/contextAPI/Sends.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { useSample } from '../contexts/sample';
 
 class Sends extends Component {
 
     state = {
         input: ''
+    }
+
+    componentDidMount() {
+        // 초기 값 설정
+        this.setState({
+            input: this.props.value,
+        })
     }
 
     handleChange = (e) => {
@@ -12,7 +20,8 @@ class Sends extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        // 구현 예정
+        // props로 받은 setValue 호출
+        this.props.setValue(this.state.input);
     }
 
     render() {
@@ -25,4 +34,5 @@ class Sends extends Component {
     }
 }
 
-export default Sends;
+// useSample 사용
+export default useSample(Sends);
